a7f6b1ea0ff0d9b3c71e52bfb324f763c8bd4454
Kid A <thisismeamir@outlook.com>
2024-10-05 16:36:15 +0330

    [Publishment] iskportal.com publishment methods checking before initial publishing.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import "./assets/styling/main.css"
 import { createApp } from 'vue'
 import App from './App.vue'
-import {createRouter, useRouter} from 'vue-router'
-import {use} from "marked";
 import {router} from "@/routes/router.ts";
 
 createApp(App)
