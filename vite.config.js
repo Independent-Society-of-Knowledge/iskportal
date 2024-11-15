a2b513651860bd2192b09eb039578077211ee906
Kid A <thisismeamir@outlook.com>
2024-11-15 18:53:56 +0330

    [Implementation] links

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(), svgLoader()
   ],
-  base: '/iskportal',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
